ba478f054c412a6163656f890bdc1ce44a7aeeb9
Jan Eirik Sandnes <github@janeirik.no>
2010-09-03 19:45:59 +0200

    Added rewrite/redirect support

--- inc/config.php
+++ inc/config.php
@@ -1,21 +1,21 @@
 <?
 /* Squid rewrite script, written by Jan Eirik Sandnes
- * 
- */
+*
+*/
 
 
 //SQL Configuration here please.
 
 define('SQL_HOST', 'localhost');
-define('SQL_USER', 'root');
-define('SQL_PASS', 'eoidt668');
-define('SQL_DB', 'proxy'); 
+define('SQL_USER', 'proxy');
+define('SQL_PASS', 'kebab');
+define('SQL_DB', 'proxy');
 
 
 
 // Wanna know what she's doin'?
 define('LOG_URLS', TRUE);
-define('DEBUG',	TRUE);
+define('DEBUG', TRUE);
 define('LOG_FILE', 'sre.log');
 
 ?>
